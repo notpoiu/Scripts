97f94d9b11211ed859e704f2d597fb48437eba9b
upio <75510171+notpoiu@users.noreply.github.com>
2024-10-17 22:21:53 -0400

    Update backdoor.lua

--- mspaint/backdoor.lua
+++ mspaint/backdoor.lua
@@ -1,2 +1,2 @@
-print("mspaint backdoor has been depricated, using mspaint v2 loadstring instead...")
+print("mspaint backdoor has been depricated, using mspaint v3 loadstring instead...")
 loadstring(game:HttpGet("https://raw.githubusercontent.com/notpoiu/mspaint/main/main.lua"))()
