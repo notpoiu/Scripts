3af7532c340710a567d26b265b6fd031384ea16a
upio <75510171+notpoiu@users.noreply.github.com>
2024-11-21 09:06:22 -0500

    refactor: add support for table arguments

--- utils/console/main.lua
+++ utils/console/main.lua
@@ -1,73 +1,122 @@
 --[[
 ©️ 2024 upio
 
-ConsoleUtils.lua, a utility made to help with logging dynamic messages in roblox console.
-https://mshub.mstudio45.com/
+Console Utils, a utility made to help with logging dynamic messages in roblox console.
 https://www.upio.dev/
+https://www.mspaint.cc/
 
 Please do not redistribute or claim the code as your own.
 However you may use it anywhere without any credits (but credits are appreciated <3)
 
 ]]
 
-if not getgenv()._console_message_counter then
-    getgenv()._console_message_counter = 3000
+local global_env = getgenv() or shared or _G or {}
+local cloneref = (cloneref or clonereference or function(instance: any) return instance end)
+
+local RunService = cloneref(game:GetService("RunService"))
+local LogService = cloneref(game:GetService("LogService"))
+local CoreGui = cloneref(game:GetService("CoreGui"))
+
+if not global_env._console_message_counter then
+    global_env._console_message_counter = 3000
 end
 
 local module = {}
 
-function module.custom_print(msg, img, clr)
-    getgenv()._console_message_counter = getgenv()._console_message_counter + 1
-    
+function _internal_get_guid()
+    global_env._console_message_counter = global_env._console_message_counter + 1
+    return tostring(global_env._console_message_counter) .. tostring(tick())
+end
+
+function _internal_get_message_index(UMID)
     local message_index = -1
-    local MSG_GUID = tostring(_console_message_counter)
-
-    local timestamp = ""
-
-    local message = msg
-    local image = img or ""
-    local color = clr or Color3.fromRGB(255, 255, 255)
-
-    print(MSG_GUID)
     
-    task.delay(0.05, function()
-        repeat task.wait(.15)
-            for i, data in pairs(game:GetService("LogService"):GetLogHistory()) do
-                if tostring(data.message) == tostring(MSG_GUID) then
-                    message_index = i+1
-                    break
-                end
-            end
-        until message_index ~= -1    
-    end)
+    repeat task.wait(.05)
+        for idx, data in pairs(LogService:GetLogHistory()) do
+            if tostring(data.message) ~= tostring(UMID) then continue end
+            
+            message_index = idx + 1
+            break
+        end
+    until message_index ~= -1
 
-    local function is_cons_logs_open()
-        local console_master = game:GetService("CoreGui"):FindFirstChild("DevConsoleMaster")
+    return message_index
+end
+
+function _internal_is_console_open()
+    local console_master = CoreGui:FindFirstChild("DevConsoleMaster")
         
-        if not console_master then
-            return false
-        end
-
-        local window = console_master:FindFirstChild("DevConsoleWindow")
-
-        if not window then
-            return false
-        end
-
-        local dev_console_ui = window:FindFirstChild("DevConsoleUI")
-
-        if not dev_console_ui then
-            return false
-        end
-
-        return (dev_console_ui:FindFirstChild("MainView") and dev_console_ui.MainView:FindFirstChild("ClientLog"))
+    if not console_master then
+        return false
     end
 
-    local conn; conn = game:GetService("RunService").RenderStepped:Connect(function()
-        local is_console_open = is_cons_logs_open()
+    local window = console_master:FindFirstChild("DevConsoleWindow")
 
-        if is_console_open then
-            local ConsoleUI = game:GetService("CoreGui").DevConsoleMaster.DevConsoleWindow.DevConsoleUI
+    if not window then
+        return false
+    end
+
+    local dev_console_ui = window:FindFirstChild("DevConsoleUI")
+
+    if not dev_console_ui then
+        return false
+    end
+
+    return (dev_console_ui:FindFirstChild("MainView") and dev_console_ui.MainView:FindFirstChild("ClientLog"))
+end
+
+function module.custom_print(...)
+    local message = ""
+    local image = ""
+    local color = Color3.fromRGB(255, 255, 255)
+    local timestamp = os.date("%H:%M:%S")
+
+    if typeof(select(1, ...)) == "table" then
+        local data = select(1, ...)
+
+        if typeof(data.message) == "string" then
+            message = data.message
+        end
+
+        if typeof(data.image) == "string" then
+            image = data.image
+        end
+
+        if typeof(data.color) == "Color3" then
+            color = data.color
+        end
+
+    else
+        local msg = select(1, ...)
+        local img = select(2, ...)
+        local clr = select(3, ...)
+
+        if typeof(msg) == "string" then
+            message = msg
+        end
+
+        if typeof(img) == "string" then
+            image = img
+        end
+        
+        if typeof(clr) == "Color3" then
+            color = clr
+        end
+    end
+    
+    -- unique message id
+    local UMID = _internal_get_guid()
+    print(UMID)
+    
+    local message_index = _internal_get_message_index(UMID)
+    
+    local ConsoleUI;
+    local conn; conn = RunService.RenderStepped:Connect(function()
+        if _internal_is_console_open() then
+            if not ConsoleUI or not ConsoleUI.Parent or not ConsoleUI:IsDescendantOf(CoreGui) then
+                ConsoleUI = CoreGui.DevConsoleMaster.DevConsoleWindow.DevConsoleUI
+            end
+            
             local log = ConsoleUI.MainView.ClientLog:FindFirstChild(tostring(message_index))
             
             if not log then
@@ -81,10 +130,6 @@ function module.custom_print(msg, img, clr)
                 return
             end
 
-            if timestamp == "" then
-                timestamp = msg.Text:sub(1, 8)
-            end
-
             msg.Text = timestamp .. " -- " .. message
             msg.TextColor3 = color
 
@@ -95,10 +140,40 @@ function module.custom_print(msg, img, clr)
 
     local log_module = {}
 
-    function log_module.update_message(_message, _image, _color)
-        message = _message or message
-        image = _image or image
-        color = _color or color
+    function log_module.update_message(...)
+        if typeof(select(1, ...)) == "table" then
+            local data = select(1, ...)
+
+            if typeof(data.message) == "string" then
+                message = data.message
+            end
+
+            if typeof(data.image) == "string" then
+                image = data.image
+            end
+
+            if typeof(data.color) == "Color3" then
+                color = data.color
+            end
+        else
+            local msg = select(1, ...)
+            local img = select(2, ...)
+            local clr = select(3, ...)
+
+            if typeof(msg) == "string" then
+                message = msg
+            end
+
+            if typeof(img) == "string" then
+                image = img
+            end
+            
+            if typeof(clr) == "Color3" then
+                color = clr
+            end
+        end
+
+        timestamp = os.date("%H:%M:%S")
     end
 
     function log_module.cleanup()
@@ -107,6 +182,7 @@ function module.custom_print(msg, img, clr)
 
     return log_module
 end
+
 
 function module.custom_console_progressbar(params)
     if typeof(params) == "string" then
